4fd45e7e157ec30e62eb7bc90afb35515b26f708
Priyank <text2priyank@gmail.com>
2021-08-23 12:52:11 -0500

    Sample app

---
+++ src/main/java/com/priyank/app/DemoController.java
@@ -0,0 +1,19 @@
+package com.priyank.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "homepage";
+    }
+}
